7cca9806c21548028939c04aa89ce381265b74e3
meerhamza565 <68352913+meerhamza565@users.noreply.github.com>
2020-07-22 02:36:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var a = prompt("Enter your Name", "your Name is");
+alert(a);
+
+var b = +prompt("Enter your age");
+if( b >= 15){
+    alert("you are elegebal for this website");
+}
+else{
+    alert("your age going to school");
+}
